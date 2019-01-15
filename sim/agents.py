83415476b7edc7d44c02f753ee97b9680d5f37ef
Benamira Adrien <adrien.benamira@supelec.fr>
2019-01-15 19:29:47 +0100

    DQN except learning

--- sim/agents.py
+++ sim/agents.py
@@ -1,10 +1,15 @@
 import torch
+import numpy as np
+import random
+from torch.optim import Adam
+from model.DQN import DQN_unit
+from utils.config import Config
 
+config = Config('./builds')
 
 class Agent:
     type = "prey"  # or predator
     id = 0
-
     # For RL
     gamma = 0.9
     epsilon_greedy = 0.01
@@ -63,11 +68,34 @@ class Agent:
 class AgentDQN(Agent):
     def __init__(self, type, agent_id, agent_config):
         super(AgentDQN, self).__init__(type, agent_id, agent_config)
+        self.policy_net = DQN_unit()
+        self.target_net = DQN_unit()
+        self.policy_optimizer = Adam(self.policy_net.parameters(), lr=config.agents.lr)
+        self.hard_update(self.target_policy, self.policy)
 
-        self.policy_net = None
-        self.target_net = None
+    def hard_update(self, target, policy):
+        """
+        Copy network parameters from source to target
+
+        """
+        target.load_state_dict(policy.state_dict())
 
     def draw_action(self, observation):
-        action = 0
+        p = np.random.random()
+        if p < self.eps_greedy:
+            action_probs = self.policy_net(observation)
+            action = np.argmax(action_probs[0])
+        else:
+            action = random.randrange(self.n_actions)
         return action
+
+    def load(self, name):
+        self.eval_network.load_weights(name)
+        self.update_target_weights()
+
+    def save(self, name):
+        self.eval_network.save_weights(name)
+
+    def learn(self, batch):
+        pass
 
