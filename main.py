06e67870b2baf2490258a6a19749a1fe6417dfce
bdvllrs <bdvllrs@gmail.com>
2019-01-15 21:57:15 +0100

    plot losses, save and load trained models

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from datetime import datetime
 
 import matplotlib.pyplot as plt
 import torch
+import numpy as np
 from sim import Env, AgentDQN, ReplayMemory
-from utils import Config
-
+from utils import Config, Metrics, compute_discounted_return
 
 plt.ion()
 
@@ -16,7 +16,7 @@ device = torch.device(device_type)
 
 print("Using", device_type)
 
-model_path = config.learning.save_folder + '/' + datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+model_path = os.path.abspath(config.learning.save_folder + '/' + datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
 os.makedirs(model_path)
 
 number_agents = config.agents.number_predators + config.agents.number_preys
@@ -26,9 +26,18 @@ agents = [AgentDQN("predator", "predator-{}".format(k), device, config.agents)
 agents += [AgentDQN("prey", "prey-{}".format(k), device, config.agents)
            for k in range(config.agents.number_preys)]
 
+metrics = []
+
 # Definition of the memories and set to device
+# Define the metrics for all agents
 for agent in agents:
     agent.memory = ReplayMemory(config.replay_memory.size)
+    metrics.append(Metrics())
+
+    # If we have to load the pretrained model
+    if config.learning.use_model:
+        path = os.path.abspath(os.path.join(config.learning.model_path, agent.id + ".pth"))
+        agent.load(path)
 
 env = Env(config.env)
 
@@ -39,22 +48,24 @@ for agent in agents:
 fig_board = plt.figure(0)
 ax_board = fig_board.gca()
 
-fig_losses = plt.figure(1)
-ax_losses = fig_losses.gca()
+fig_losses_returns, (ax_losses, ax_returns) = plt.subplots(2, 0)
 
 plt.show()
 
-
 for episode in range(config.learning.n_episodes):
+    all_rewards = []
     states = env.reset()
     terminal = False
     while not terminal:
-        actions = [0 for i in range(number_agents)]  # TODO
+        actions = [agents[i].draw_action(states[i]) for i in range(len(agents))]
         next_states, rewards, terminal = env.step(states, actions)
-        # Plot environment
-        env.plot(states, ax_board)
-        plt.draw()
-        plt.pause(0.0001)
+        all_rewards.append(rewards)
+
+        if not episode % config.learning.plot_episodes_every:
+            # Plot environment
+            env.plot(states, ax_board)
+            plt.draw()
+            plt.pause(0.0001)
 
         # Learning Step
         for k in range(len(agents)):
@@ -64,5 +75,28 @@ for episode in range(config.learning.n_episodes):
             batch = agents[k].memory.get_batch(config.learning.batch_size, shuffle=config.replay_memory.shuffle)
             # Learn
             loss = agents[k].learn(batch)
+            metrics[k].add_loss(loss)
 
         states = next_states
+
+    # Compute discounted return of the episode
+    for k in range(len(agents)):
+        reward = [all_rewards[i][k] for i in range(len(all_rewards))]
+        discounted_return = compute_discounted_return(config.agents.gamma, reward)
+        metrics[k].add_return(discounted_return)
+
+    # Plot learning curves
+    if not episode % config.learning.plot_curves_every:
+        for k in range(len(agents)):
+            # Compute average of losses of all learning step in episode and add it to the list of losses
+            metrics[k].compute_loss_average()
+
+            metrics[k].plot_losses(ax_losses, legend=agents[k].id)
+            metrics[k].plot_returns(ax_returns, legend=agents[k].id)
+        plt.draw()
+        plt.pause(0.0001)
+
+    # Save models
+    for agent in agents:
+        path = os.path.join(model_path, agent.id + ".pth")
+        agent.save(path)
--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,7 @@
-
-class Metrics:
-    def __init__(self):
-        self.data = {
-            "losses": [],
-            "returns": []
-        }
+def compute_discounted_return(gamma, rewards):
+    discounted_return = 0
+    discount = 1
+    for reward in rewards:
+        discounted_return += reward * discount
+        discount *= gamma
+    return discounted_return
