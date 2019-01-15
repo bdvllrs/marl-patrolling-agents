b98682eab48f3ec54300806d4d07d46ace60d2c4
bdvllrs <bdvllrs@gmail.com>
2019-01-15 18:14:34 +0100

    typo + start main

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+from sim import Env, AgentDQN, ReplayMemory
 from utils import Config
 
 config = Config('config/')
+
+# Definition of the agents
+agents = [AgentDQN("predator", "predator-{}".format(k), config.agents) for k in range(config.agents.number_predators)]
+agents += [AgentDQN("prey", "prey-{}".format(k), config.agents) for k in range(config.agents.number_preys)]
+
+
