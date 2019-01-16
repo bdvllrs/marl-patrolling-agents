57a2a951e5bae6bccc23815d42c30d8893c9598e
bdvllrs <bdvllrs@gmail.com>
2019-01-16 09:51:41 +0100

    change reward function

--- sim/rewards.py
+++ sim/rewards.py
@@ -1,7 +1,8 @@
-from typing import List
+from typing import List, Tuple
 from sim.agents import Agent
 import numpy as np
 from utils.config import Config
+from utils import get_enemy_positions
 
 config = Config('./config')
 
@@ -16,30 +17,31 @@ def reward_full(observations, agents: List[Agent], t):
     """
     all_winners = []
     all_rewards = []
-    predator_lost = False
+    predator_lost = True
     for idx, agent in enumerate(agents):
-        (rw, win) = reward_full_agent(observations, idx, agents, t)
-        if agent.type == "prey" and win:
-            predator_lost = True
-        all_winners.append(win)
+        rw, is_winner = get_reward_agent(observations, idx, agents, t)
+        if agent.type == "predator" and is_winner:
+            predator_lost = False
+
+        all_winners.append(is_winner)
         all_rewards.append(rw)
-    for idx, agent in enumerate(agents):
-        if predator_lost:
-            if agent.type == "prey":
-                if not all_winners[idx]:
-                    all_rewards[idx] = -1
-        else:
-            if agent.type == "predator":
-                if all_winners[idx]:
-                    all_rewards[idx] = 1
-                else:
-                    all_rewards[idx] = 0.8
+    if config.reward.share_wins_among_predators:
+        for idx, agent in enumerate(agents):
+            # if predator_lost:
+            #     if agent.type == "prey":
+            #         if not all_winners[idx]:
+            #             all_rewards[idx] = -1
+            # else:
+            if not predator_lost:  # Give incentive to other predators if one wins
+                if agent.type == "predator":
+                    if not all_winners[idx]:
+                        all_rewards[idx] = config.reward.reward_if_predators_win
     return all_rewards
 
 
-def reward_full_agent(observations, agent_index, agents: List[Agent], t):
+def get_reward_agent(observations, agent_index, agents: List[Agent], t):
     """
-    reward for 1 action and 1 agent at time t
+    Reward for 1 action and 1 agent at time t
     :param observations:
     :param agent_index:
     :param agents:
@@ -48,36 +50,55 @@ def reward_full_agent(observations, agent_index, agents: List[Agent], t):
     """
     x = observations[2 * agent_index]
     y = observations[2 * agent_index + 1]
-
-    min_dist = None
-    rew_d = None
+    min_distance = None
+    agent_reward = None
     winner = False
-    if agents[agent_index].type == "predator":
-        for idx, agent in enumerate(agents):
-            if agent.type == "prey":
-                rew, dis = distance_reward(x, y, observations, idx)
-                if min_dist is None or dis < min_dist:
-                    rew_d = rew
-                    min_dist = dis
-                if dis <= 1 / config.env.board_size:
-                    winner = True
-    else:
-        for idx, agent in enumerate(agents):
-            if agent.type == "predator":
-                rew, dis = distance_reward(x, y, observations, idx)
-                # rew_d = 1 - rew
-                if min_dist is None or dis < min_dist:
-                    rew_d = 1 - rew
-                    min_dist = dis
-                if dis > 1 / config.env.board_size:
-                    winner = True
+    agent_type = agents[agent_index].type
 
-    return rew_d, winner
+    # For all enemies, we compute the distance between actual agent and enemy agents
+    for x_1, y_1 in get_enemy_positions(agent_index, agents, observations):
+        reward, distance = distance_reward(agent_type, x, y, x_1, y_1)
+        # If distance is smaller...
+        if min_distance is None or distance < min_distance:
+            min_distance, agent_reward = distance, reward
+
+    # Determine if it's a winner
+    if ((agent_type == "predator" and min_distance <= 1 / config.env.board_size) or
+            (agent_type == "prey" and min_distance > 1 / config.env.board_size)):
+        winner = True
+
+    return agent_reward, winner
 
 
-def distance_reward(x, y, observations, idx):
-    x1 = observations[2 * idx]
-    y1 = observations[2 * idx + 1]
+def distance_reward(agent_type, x, y, x1, y1) -> Tuple:
+    """
+    Returns the reward obtained according to the type and the position of two agents
+    Args:
+        agent_type: between prey and predator
+        x: position x current agent
+        y: position y current agent
+        x1: position secondary agent
+        y1: position secondary agent
+
+    Returns: (reward, distance_between_agents)
+    """
+    assert agent_type in ['prey', 'predator'], "Agent type is not correct."
+    return distance_reward_prey(x, y, x1, y1) if agent_type == "prey" else distance_reward_predator(x, y, x1, y1)
+
+
+def distance_reward_prey(x, y, x1, y1):
+    """
+    Returns: Reward = $1 - \exp(-c \cdot d^2)$
+    """
     distance = np.linalg.norm([x - x1, y - y1])
-    rw = np.exp(-config.reward.coef_distance_reward * distance**2)
-    return (rw, distance)
+    rw = np.exp(-config.reward.coef_distance_reward * distance * distance)
+    return 1 - rw, distance
+
+
+def distance_reward_predator(x, y, x1, y1):
+    """
+    Returns: Reward = $\exp(-c \cdot d^2)$
+    """
+    distance = np.linalg.norm([x - x1, y - y1])
+    rw = np.exp(-config.reward.coef_distance_reward * distance * distance)
+    return rw, distance
--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,34 @@
+from typing import List, Tuple
+from sim.agents import Agent
+
+
 def compute_discounted_return(gamma, rewards):
+    """
+    Args:
+        gamma: discount factor.
+        rewards: List of reward for the episode
+
+    Returns: $\Sum_{t=0}^T \gamma^t r_t$
+
+    """
     discounted_return = 0
     discount = 1
     for reward in rewards:
         discounted_return += reward * discount
         discount *= gamma
     return discounted_return
+
+
+def get_enemy_positions(agent_index: int, agents: List[Agent], positions: List[float]) -> List[Tuple[float, float]]:
+    """
+    Returns the list of agents enemy positions
+    Args:
+        positions:
+        agent_index:
+        agents:
+    Returns:
+    """
+    for k in range(len(agents)):
+        if agents[k].type != agents[agent_index].type:
+            x, y = positions[2 * k], positions[2 * k + 1]
+            yield (x, y)
