71a0892b508d6ddc4e9b96880e25a86f5ba5e1d8
bdvllrs <bdvllrs@gmail.com>
2019-01-16 13:47:44 +0100

    fix plotting new state

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ for episode in range(config.learning.n_episodes):
         if not episode % config.learning.plot_episodes_every:
             # Plot environment
             ax_board.cla()
-            env.plot(states, rewards, ax_board)
+            env.plot(next_states, rewards, ax_board)
             plt.draw()
             plt.pause(0.01)
 
