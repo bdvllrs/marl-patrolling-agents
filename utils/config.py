263db7175471059ef5b456a53a789827a6703df3
bdvllrs <bdvllrs@gmail.com>
2019-01-16 11:18:37 +0100

    update default config

--- utils/config.py
+++ utils/config.py
@@ -17,8 +17,6 @@ class Config:
         self.__data = config if config is not None else {}
         if path is not None:
             self.__path = os.path.abspath(os.path.join(os.curdir, path))
-            with open(os.path.join(self.__path, "config.yaml"), "rb") as default_config:
-                self.__data.update(yaml.load(default_config))
             for config in sorted(os.listdir(self.__path)):
                 if config != "defaulf.yaml" and config[-4:] in ["yaml", "yml"]:
                     with open(os.path.join(self.__path, config), "rb") as config_file:
