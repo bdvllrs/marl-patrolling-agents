add train and step step + fix no gradient in maddpg

main_maddpg.py
@@ -9,12 +9,11 @@ import torch
 import numpy as np
 from sim import Env, ReplayMemory
 from sim.agents.multiagents import AgentMADDPG
-from utils import Config, Metrics, compute_discounted_return
+from utils import Config, Metrics, compute_discounted_return, train, test
 
 plt.ion()
 
 config = Config('config/')
-
 
 device_type = "cuda" if torch.cuda.is_available() and config.learning.cuda else "cpu"
 device = torch.device(device_type)
@@ -55,17 +54,19 @@ shared_memory = ReplayMemory(config.replay_memory.size)
 # Add agents to the environment
 for idx, agent in enumerate(agents):
     env.add_agent(agent, position=None)
+    agent.add_agents(agents, idx)
 
-fig_board = plt.figure(0)
+fig_board = plt.figure(0, figsize=(10, 10))
 if config.env.world_3D:
     ax_board = fig_board.gca(projection="3d")
 else:
     ax_board = fig_board.gca()
 
-fig_losses_returns, ((ax_losses, ax_losses_actor), (ax_returns, ax_collisions)) = plt.subplots(2, 2)
+fig_losses_returns, ((ax_losses, ax_losses_actor), (ax_returns, ax_collisions)) = plt.subplots(2, 2, figsize=(20, 10))
 
 plt.show()
 
+action_dim = 7 if config.env.world_3D else 5
 start = time.time()
 path_figure_episode = None
 progress_bar = None
@@ -74,60 +75,35 @@ for episode in range(config.learning.n_episodes):
         if progress_bar is not None:
             progress_bar.close()
         progress_bar = tqdm(total=config.learning.plot_episodes_every)
-    test_step = False
-    if not episode % config.learning.plot_episodes_every:
-        test_step = True
-    if not episode % config.learning.save_episodes_every and config.save_build:
-        path_figure_episode = os.path.join(path_figure, "episode-{}".format(episode))
-        os.mkdir(path_figure_episode)
-    all_rewards = []
-    states = env.reset()
-    terminal = False
-    step_k = 0
-    while not terminal:
-        actions = []
-        for i in range(len(agents)):
-            action = agents[i].draw_action(states[i], no_exploration=test_step)
-            actions.append(action)
-        next_states, rewards, terminal, n_collisions = env.step(states, actions)
-        collision_metric.add_collision_count(n_collisions)
-        all_rewards.append(rewards)
 
-        if not episode % config.learning.plot_episodes_every or not episode % config.learning.save_episodes_every:
+    # Test step
+    if not episode % config.learning.test_every:
+        for test_episode in range(config.learning.n_episode_in_test):
+            test(env, agents, collision_metric, metrics, config)
+
+    # Plot step
+    if not episode % config.learning.plot_episodes_every or not episode % config.learning.save_episodes_every:
+        all_states, all_rewards = test(env, agents, collision_metric, metrics, config)
+
+        # Make path for episode images
+        if not episode % config.learning.save_episodes_every and config.save_build:
+            path_figure_episode = os.path.join(path_figure, "episode-{}".format(episode))
+            os.mkdir(path_figure_episode)
+
+        # Plot last test episode
+        for k, (states, rewards) in enumerate(zip(all_states, all_rewards)):
             # Plot environment
             ax_board.cla()
             env.plot(states, rewards, ax_board)
             plt.draw()
             if not episode % config.learning.save_episodes_every and config.save_build:
-                fig_board.savefig(os.path.join(path_figure_episode, "frame-{}.jpg".format(step_k)))
+                fig_board.savefig(os.path.join(path_figure_episode, "frame-{}.jpg".format(k)))
                 fig_losses_returns.savefig(os.path.join(path_figure, "losses.eps"), dpi=1000, format="eps")
             if not episode % config.learning.plot_episodes_every:
                 plt.pause(0.001)
 
-        step_k += 1
-
-        shared_memory.add(states, next_states, actions, rewards)
-
-        # Learning step
-        # Get batch for learning (batch_size x n_agents x dim)
-        batch = shared_memory.get_batch(config.learning.batch_size, shuffle=config.replay_memory.shuffle)
-        target_actors = [agents[k].target_actor for k in range(len(agents))]
-
-        if batch is not None:
-            for k in range(len(agents)):
-                loss_critic = agents[k].learn_critic(batch, target_actors, k)
-                metrics[k].add_loss(loss_critic)
-            for k in range(len(agents)):
-                loss_actor = agents[k].learn_actor(batch, target_actors, k)
-                metrics[k].add_loss_actor(loss_actor)
-
-        states = next_states
-
-    # Compute discounted return of the episode
-    for k in range(len(agents)):
-        reward = [all_rewards[i][k] for i in range(len(all_rewards))]
-        discounted_return = compute_discounted_return(config.agents.gamma, reward)
-        metrics[k].add_return(discounted_return)
+    all_states, all_next_states, all_rewards, all_actions = train(env, agents, shared_memory,
+                                                                  metrics, action_dim, config)
 
     # Plot learning curves
     if not episode % config.learning.plot_curves_every:
@@ -136,6 +112,7 @@ for episode in range(config.learning.n_episodes):
         ax_losses.cla()
         ax_returns.cla()
         ax_losses_actor.cla()
+        ax_collisions.cla()
         for k in range(len(agents)):
             metrics[k].compute_averages()
 

model/dqn.py
@@ -34,18 +34,18 @@ class DQNCritic(nn.Module):
     def __init__(self):
         super(DQNCritic, self).__init__()
 
-        # action_dim = 7 if config.env.world_3D else 5
+        action_dim = 7 if config.env.world_3D else 5
         n_agents = config.agents.number_preys + config.agents.number_predators
         n_obstacles = 2 * len(config.env.obstacles)
         state_dim = n_agents * 3 + n_obstacles + int(config.env.magic_switch) * (2 + n_agents)
         self.fc = nn.Sequential(
-            nn.Linear(state_dim + n_agents, 1024),
+            nn.Linear(state_dim + n_agents * action_dim, 1024),
             nn.ReLU(),
             nn.Linear(1024, 128),
             nn.ReLU(),
             nn.Linear(128, 32),
             nn.ReLU(),
-            nn.Linear(32, 1),
+            nn.Linear(32, 1)
         )
 
     def forward(self, x, actions):
@@ -74,8 +74,7 @@ class DQNActor(nn.Module):
             nn.ReLU(),
             nn.Linear(64, 16),
             nn.ReLU(),
-            nn.Linear(16, action_dim),
-            nn.Softmax(dim=1)
+            nn.Linear(16, action_dim)
         )
 
     def forward(self, x):

sim/agents/multiagents.py
@@ -34,8 +34,14 @@ class AgentMADDPG(Agent):
         self.target_actor.eval()
 
         self.steps_done = 0
+        self.agents = None
+        self.current_agent_idx = None
 
-    def draw_action(self, state, no_exploration):
+    def add_agents(self, agents, idx):
+        self.agents = agents
+        self.current_agent_idx = idx
+
+    def draw_action(self, state, no_exploration=False):
         eps_threshold = self.EPS_END + (self.EPS_START - self.EPS_END) * math.exp(
             -1. * self.steps_done / self.EPS_DECAY)
         with torch.no_grad():
@@ -51,31 +57,31 @@ class AgentMADDPG(Agent):
         self.steps_done += 1
         return action
 
-    def learn_critic(self, batch, target_actors, idx):
+    def learn_critic(self, batch):
         """
-
         :param batch:
         :return:
         """
         state_batch, next_state_batch, action_batch, reward_batch = batch
 
-        state_batch = torch.FloatTensor(state_batch[:, idx]).to(self.device)  # batch x agents x dim
-        next_state_batch = torch.FloatTensor(next_state_batch[:, idx]).to(self.device)
-        action_batch = torch.FloatTensor(action_batch).to(self.device).unsqueeze(2)  # batch x agents x 1
-        reward_batch = torch.FloatTensor(reward_batch[:, idx]).to(self.device)  # batch x dim
+        state_batch = torch.FloatTensor(state_batch[:, self.current_agent_idx]).to(self.device)  # batch x dim
+        next_state_batch = torch.FloatTensor(next_state_batch[:, self.current_agent_idx]).to(self.device)
+        action_batch = torch.FloatTensor(action_batch).to(self.device)  # batch x agents x action_dim
+        reward_batch = torch.FloatTensor(reward_batch[:, self.current_agent_idx]).to(self.device)  # batch x 1
+
+        # Add random noise
+        action_batch = action_batch + torch.normal(torch.zeros_like(action_batch),
+                                                   torch.zeros_like(action_batch).fill_(0.1))
+        action_batch = torch.clamp(action_batch, 0, 1)
 
         self.critic_optimizer.zero_grad()
 
-        action_dim = 7 if config.env.world_3D else 5
-
         target_actions = []
         policy_actions = []
-        for a in range(len(target_actors)):
-            target_action = target_actors[a](next_state_batch).max(1)[1].unsqueeze(1).to(torch.float)
-            # onehot_target_action = to_onehot(target_action, action_dim)
-            # onehot_policy_action = to_onehot(action_batch[:, a], action_dim)
-            target_actions.append(target_action / float(action_dim))
-            policy_actions.append(action_batch[:, a] / float(action_dim))
+        for a in range(len(self.agents)):
+            target_action = self.agents[a].target_actor(next_state_batch)
+            target_actions.append(target_action)
+            policy_actions.append(action_batch[:, a])
 
         predicted_q = self.policy_critic(state_batch, policy_actions)  # dim (batch_size x 1)
         target_q = reward_batch + self.gamma * self.target_critic(next_state_batch, target_actions)
@@ -91,23 +97,21 @@ class AgentMADDPG(Agent):
 
         return loss.detach().cpu().item()
 
-    def learn_actor(self, batch, target_actors, idx):
+    def learn_actor(self, batch):
         state_batch, next_state_batch, action_batch, reward_batch = batch
 
-        state_batch = torch.FloatTensor(state_batch[:, idx]).to(self.device)  # batch x agents x dim
-        action_batch = torch.FloatTensor(action_batch).to(self.device).unsqueeze(2)  # batch x agents x 1
-
-        action_dim = 7 if config.env.world_3D else 5
+        state_batch = torch.FloatTensor(state_batch[:, self.current_agent_idx]).to(self.device)  # batch  x dim
+        action_batch = torch.FloatTensor(action_batch).to(self.device)  # batch x agents x action_dim
 
         self.actor_optimizer.zero_grad()
-        predicted_action = self.policy_actor(state_batch).max(1)[1].unsqueeze(1)
-        cloned_action_batch = action_batch.clone()
-        cloned_action_batch[:, idx] = predicted_action
+        predicted_action = self.policy_actor(state_batch)
 
         policy_actions = []
-        for a in range(len(target_actors)):
-            # onehot_policy_action = to_onehot(cloned_action_batch[:, a], action_dim)
-            policy_actions.append(cloned_action_batch[:, a] / float(action_dim))
+        for a in range(len(self.agents)):
+            if a == self.current_agent_idx:
+                policy_actions.append(predicted_action)
+            else:
+                policy_actions.append(action_batch[:, a])
 
         actor_loss = -self.policy_critic(state_batch, policy_actions)
         actor_loss = actor_loss.mean()

sim/rewards.py
@@ -125,5 +125,5 @@ def distance_reward_predator(dx, dy, dz):
     Returns: Reward = $\exp(-c \cdot d^2)$
     """
     distance = np.linalg.norm([dx, dy, dz])
-    rw = -1 + 2 * np.exp(-config.reward.coef_distance_reward_predator * distance * distance)
+    rw = np.exp(-config.reward.coef_distance_reward_predator * distance * distance)
     return rw, distance